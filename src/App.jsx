b7958c46a9190d60b881c4163e0c91f7b9415a37
Eneas Junior <eneas.silva.jr@gmail.com>
2023-06-12 21:41:26 -0300

    importing objects with people information properties and setting it up as a state variable of useState hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-  const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
-};
-export default App;
+import { useState } from 'react'
+import data from './data'
+
+const App = () => {
+  const [people, setPeople] = useState(data)
+  console.log(people)
+  return <h2>Birthday Reminder - Starter</h2>
+}
+export default App
