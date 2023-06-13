d54d6152a1093561377c8e08f91f7de83ec42efd
Eneas Junior <eneas.silva.jr@gmail.com>
2023-06-13 15:25:11 -0300

    Creating file compolent Person.jsx to return it and render it within the section from componetn List

--- src/List.jsx
+++ src/List.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Person from './Person'
 
 const List = ({ people }) => {
   return (
     <section>
       {people.map((person) => {
-        console.log(person)
+        return <Person key={person.id} {...person} />
       })}
     </section>
   )
